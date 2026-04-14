8adb7dc1bd4cc7a6b7303c2b24b500ecdf38762f
Caelan208 <c.verhoef@student.alfa-college.nl>
2026-04-14 10:45:35 +0200

    script updates and start page finished

--- script/script.js
+++ script/script.js
@@ -23,8 +23,8 @@ const platforms = [
 
 // Teken stickman speler
 function drawPlayer() {
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = 2;
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 3;
 
   // Hoofd
   ctx.beginPath();
@@ -62,6 +62,78 @@ function drawPlatforms() {
   });
 }
 
+// Update speler positie en collision
+function updatePlayer() {
+  // Input
+  if (keys.left) velocityX = -speed;
+  else if (keys.right) velocityX = speed;
+  else velocityX = 0;
+
+  if (keys.up && onGround) {
+    velocityY = jumpStrength;
+    onGround = false;
+  }
+
+  // Zwaartekracht
+  velocityY += gravity;
+
+  // Update positie
+  playerX += velocityX;
+  playerY += velocityY;
+
+  // Collision met platformen
+  onGround = false;
+  platforms.forEach(platform => {
+    if (playerX < platform[0] + platform[2] &&
+        playerX + playerWidth > platform[0] &&
+        playerY < platform[1] + platform[3] &&
+        playerY + playerHeight > platform[1]) {
+      // Bovenkant collision (landing)
+      if (velocityY > 0 && playerY < platform[1]) {
+        playerY = platform[1] - playerHeight;
+        velocityY = 0;
+        onGround = true;
+      }
+      // Onderkant (van bovenaf duwen)
+      else if (velocityY < 0 && playerY + playerHeight > platform[1] + platform[3]) {
+        playerY = platform[1] + platform[3];
+        velocityY = 0;
+      }
+      // Zij collision
+      else if (velocityX > 0 && playerX < platform[0]) {
+        playerX = platform[0] - playerWidth;
+        velocityX = 0;
+      } else if (velocityX < 0 && playerX + playerWidth > platform[0] + platform[2]) {
+        playerX = platform[0] + platform[2];
+        velocityX = 0;
+      }
+    }
+  });
+
+  // Houd speler binnen canvas
+  if (playerX < 0) playerX = 0;
+  if (playerX + playerWidth > canvas.width) playerX = canvas.width - playerWidth;
+  if (playerY + playerHeight > canvas.height) {
+    playerY = canvas.height - playerHeight;
+    velocityY = 0;
+    onGround = true;
+  }
+}
+
+// Toetsen tracking
+const keys = {};
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowLeft" || e.code === "KeyA") keys.left = true;
+  if (e.code === "ArrowRight" || e.code === "KeyD") keys.right = true;
+  if (e.code === "ArrowUp" || e.code === "KeyW" || e.code === "Space") keys.up = true;
+});
+document.addEventListener("keyup", (e) => {
+  if (e.code === "ArrowLeft" || e.code === "KeyA") keys.left = false;
+  if (e.code === "ArrowRight" || e.code === "KeyD") keys.right = false;
+  if (e.code === "ArrowUp" || e.code === "KeyW" || e.code === "Space") keys.up = false;
+});
+
+// Game loop
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   updatePlayer();
